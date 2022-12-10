4a7b52b17181f4247ab5763419a8835298cd08cc
Vuhuuduy <108417986+Vuhuuduy@users.noreply.github.com>
2022-12-10 19:45:03 +0700

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,7 +1,7 @@
 $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;  
     var pwd=document.getElementById("pwd").value;
-    if(userName=="duydeptrai" &&  pwd=="haanhxinhgirl"){ 
+    if(userName=="duydeptrai" &&  pwd=="haanhxinhgai"){ 
 		event.preventDefault();
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
